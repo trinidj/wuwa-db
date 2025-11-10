4ae73e2735d069710d85ca0645f76d41eed305fd
John Trinidad <trinidj03@protonmail.com>
2025-11-10 18:52:03 -0500

    improve: increase grid spacing on resonators page

--- app/resonators/page.tsx
+++ app/resonators/page.tsx
@@ -238,7 +238,7 @@ export default function ResonatorsPage() {
         </div>
       </header>
 
-      <main className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-4">
+      <main className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 gap-6">
         {filteredResonators.map((resonator) => {
           const assets = getResonatorAssets(resonator);
           return (
